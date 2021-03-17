90977eb26c7c3ef860fcdfe66041ac27b9f664bc
DQuinaluisa <dft.quinaluisa@yavirac.edu.ec>
2021-03-16 20:07:11 -0500

    Selector de Color y expresor dek puntero

--- provin/src/pages/Draw.js
+++ provin/src/pages/Draw.js
@@ -1,11 +1,18 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import CardMedia from '@material-ui/core/CardMedia';
 
 import Card from '@material-ui/core/Card';
 import { Grid  } from "@material-ui/core";
 import CanvasDraw from 'react-canvas-draw';
 function Draw() {
-
+    const [canvas, setBrush] = useState("#FCA5A5");
+    const [brush, setThick] = useState(20);
+    const style = {
+        width: brush + "px",
+        background: canvas,
+        marginLeft: "50%"
+      };
+      console.log(style.width);
   return (
   <div >
      <Grid container 
@@ -28,12 +35,38 @@ function Draw() {
         <Grid sm={1}>
         </Grid>
         <Grid  xs={12} sm={6}>
-        <Grid style={{ position: 'relative', top: 50,  }}>
-            <Card style={{ width: 400, border: '2px solid #14181C' }}>
+        <Grid style={{ position: 'relative', top: 20,  }}>
+        <label>Colour picker</label>
+        <br />
+            <input
+                style={{ background: { canvas } }}
+                type="color"
+                value={canvas}
+                onChange={(event) => {
+                console.log(event.target.value);
+                setBrush(event.target.value);
+                }}
+            />
+            <br />
+            <label>Brush Thickness</label>
+            
+            <div className="thickness" style={style}></div>
+            <input
+            min="2"
+            max="50"
+            type="range"
+            onChange={(event) => {
+                console.log(event.target.value);
+                setThick(event.target.value);
+            }}
+            />
+            <Card style={{ width: 600, border: '2px solid #14181C' }}>
                 <CanvasDraw
-                canvasHeight={550}
-                canvasWidth={'100%'}
-                hideGrid={true}
+                    brushColor={canvas}
+                    brushRadius={brush}
+                    canvasHeight={450}
+                    canvasWidth={'100%'}
+                    hideGrid={false}
                 
             />
             </Card>
--- provin/src/pages/Draw2.js
+++ provin/src/pages/Draw2.js
@@ -1,20 +1,59 @@
-import React, {  } from 'react';
-import Card from '@material-ui/core/Card';
-import { Grid  } from "@material-ui/core";
+import React, { useState } from 'react';
+
+import {  Grid  } from "@material-ui/core";
 import CanvasDraw from 'react-canvas-draw';
 
 function Draw2() {
-
+    const [canvas, setBrush] = useState("#FCA5A5");
+    const [brush, setThick] = useState(20);
+    const style = {
+        width: brush + "px",
+        background: canvas,
+        marginLeft: "50%"
+      };
+      console.log(style.width);
   return (
   <div style={{ backgroundColor: '#5DADEF', height: 657 }}>
         <Grid container>
             <Grid sm={1}>
+            
             </Grid>
             <Grid xs={12} sm={10}>
-            <Grid style={{ margin: 10, marginTop: 20, textAlign: 'center', }}>
+            
+            
+            <Grid style={{ margin: 10,  textAlign: 'center', }}>
+            <Grid>
+            <label>Colour picker</label>
+            <br />
+            <input
+                style={{ background: { canvas } }}
+                type="color"
+                value={canvas}
+                onChange={(event) => {
+                console.log(event.target.value);
+                setBrush(event.target.value);
+                }}
+            />
+            <br />
+            <label>Brush Thickness</label>
+            
+            <div className="thickness" style={style}></div>
+            <input
+              min="2"
+              max="50"
+              type="range"
+              onChange={(event) => {
+                console.log(event.target.value);
+                setThick(event.target.value);
+              }}
+            />
+            </Grid>
                 <h2> Dibuja</h2>
                     <CanvasDraw
-                    canvasHeight={550}
+                    
+                    brushColor={canvas}
+                    brushRadius={brush}
+                    canvasHeight={450}
                     canvasWidth={'100%'}
                     hideGrid={false}
                     style={{ border: '2px solid #14181C' }}
