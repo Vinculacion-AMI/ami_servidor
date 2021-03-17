0c5c27ea019a3c1f724887cfbbff4d1fd0161c6c
DQuinaluisa <dft.quinaluisa@yavirac.edu.ec>
2021-03-17 14:17:12 -0500

    Limpiar Lienzo

--- provin/src/pages/Draw.js
+++ provin/src/pages/Draw.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 import CardMedia from '@material-ui/core/CardMedia';
 
 import Card from '@material-ui/core/Card';
-import { Grid  } from "@material-ui/core";
+import { Grid, Button  } from "@material-ui/core";
 import CanvasDraw from 'react-canvas-draw';
 function Draw() {
     const [canvas, setBrush] = useState("#FCA5A5");
     const [brush, setThick] = useState(20);
+    const lienzo = useState(null);
+
+    const clear = () => {
+        lienzo.current.clear() 
+    }
+
     const style = {
         width: brush + "px",
         background: canvas,
@@ -36,32 +42,51 @@ function Draw() {
         </Grid>
         <Grid  xs={12} sm={6}>
         <Grid style={{ position: 'relative', top: 20,  }}>
+        
+        <Grid container>
+        <Grid sm={2} >
         <label>Colour picker</label>
         <br />
-            <input
-                style={{ background: { canvas } }}
-                type="color"
-                value={canvas}
-                onChange={(event) => {
-                console.log(event.target.value);
-                setBrush(event.target.value);
-                }}
-            />
-            <br />
-            <label>Brush Thickness</label>
-            
-            <div className="thickness" style={style}></div>
-            <input
-            min="2"
-            max="50"
-            type="range"
+        <input
+            style={{ background: { canvas } }}
+            type="color"
+            value={canvas}
             onChange={(event) => {
-                console.log(event.target.value);
-                setThick(event.target.value);
+            console.log(event.target.value);
+            setBrush(event.target.value);
             }}
-            />
+        />
+        <br />
+        <label>Brush Thickness</label>
+        
+        <div className="thickness" style={style}></div>
+        <input
+          min="2"
+          max="50"
+          type="range"
+          onChange={(event) => {
+            console.log(event.target.value);
+            setThick(event.target.value);
+          }}
+        />
+        </Grid>
+        <Grid sm={2} >
+        <Button onClick={clear}
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+
+        //style={{ borderRadius: 20 }}
+      >
+        Limpiar
+      </Button>
+        </Grid>
+
+      </Grid>
             <Card style={{ width: 600, border: '2px solid #14181C' }}>
                 <CanvasDraw
+                ref={lienzo}
                     brushColor={canvas}
                     brushRadius={brush}
                     canvasHeight={450}
--- provin/src/pages/Draw2.js
+++ provin/src/pages/Draw2.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
-import {  Grid  } from "@material-ui/core";
+import {  Grid, Button  } from "@material-ui/core";
 import CanvasDraw from 'react-canvas-draw';
 
 function Draw2() {
     const [canvas, setBrush] = useState("#FCA5A5");
     const [brush, setThick] = useState(20);
+    const lienzo = useState(null);
+
+    const clear = () => {
+        lienzo.current.clear() 
+    }
+
     const style = {
         width: brush + "px",
         background: canvas,
@@ -19,38 +25,52 @@ function Draw2() {
             
             </Grid>
             <Grid xs={12} sm={10}>
-            
-            
-            <Grid style={{ margin: 10,  textAlign: 'center', }}>
-            <Grid>
-            <label>Colour picker</label>
-            <br />
-            <input
-                style={{ background: { canvas } }}
-                type="color"
-                value={canvas}
+   
+            <Grid style={{ margin: 8  }}>
+            <Grid container>
+              <Grid sm={2} >
+              <label>Colour picker</label>
+              <br />
+              <input
+                  style={{ background: { canvas } }}
+                  type="color"
+                  value={canvas}
+                  onChange={(event) => {
+                  console.log(event.target.value);
+                  setBrush(event.target.value);
+                  }}
+              />
+              <br />
+              <label>Brush Thickness</label>
+              
+              <div className="thickness" style={style}></div>
+              <input
+                min="2"
+                max="50"
+                type="range"
                 onChange={(event) => {
-                console.log(event.target.value);
-                setBrush(event.target.value);
+                  console.log(event.target.value);
+                  setThick(event.target.value);
                 }}
-            />
-            <br />
-            <label>Brush Thickness</label>
-            
-            <div className="thickness" style={style}></div>
-            <input
-              min="2"
-              max="50"
-              type="range"
-              onChange={(event) => {
-                console.log(event.target.value);
-                setThick(event.target.value);
-              }}
-            />
+              />
+              </Grid>
+              <Grid sm={2} >
+              <Button onClick={clear}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+  
+              //style={{ borderRadius: 20 }}
+            >
+              Limpiar
+            </Button>
+              </Grid>
+
             </Grid>
                 <h2> Dibuja</h2>
                     <CanvasDraw
-                    
+                    ref={lienzo}
                     brushColor={canvas}
                     brushRadius={brush}
                     canvasHeight={450}
