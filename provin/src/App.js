ac8cde90848bd9d0978677f79f083b4aabcee8e2
Geovanny <srmaeson@outlook.com>
2021-03-18 11:55:44 -0500

    DraggableComponent creado y funcional

--- provin/src/App.js
+++ provin/src/App.js
@@ -6,6 +6,7 @@ import Draw2 from './pages/Draw2';
 import Levels from './pages/Levels';
 import Login from './pages/Login';
 import Registro from './pages/Registro';
+import JigSaw from "./pages/draggable/Jigsaw";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/levels" component={Levels} />
       <Route path="/draw" component={Draw} />
       <Route path="/draw2" component={Draw2} />
+      <Route path="/puzzle" component={JigSaw} />
      </Router>
     </div>
    
---
+++ provin/src/pages/draggable/style.js
@@ -0,0 +1,42 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  button: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  rootCard: {
+    margin: theme.spacing(1),
+    maxWidth: "90%",
+    maxHeight: "90%",
+    minHeight: "90%",
+    minWidth: "90%",
+    background: "#42C3E5",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 80,
+    color: "#FFFFFF",
+  },
+  rootGrid: {
+    flexGrow: 1,
+  },
+
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
