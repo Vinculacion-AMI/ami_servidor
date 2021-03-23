5ee162f1dbd5a89155f29735bbe67efcd905b0dc
DQuinaluisa <dft.quinaluisa@yavirac.edu.ec>
2021-03-23 18:23:06 -0500

    Draw Responsive

--- provin/src/pages/draw/Draw.js
+++ provin/src/pages/draw/Draw.js
@@ -6,14 +6,30 @@ import { Grid, Button  } from "@material-ui/core";
 import CanvasDraw from 'react-canvas-draw';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import '../../css/draw.css';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 560,
+   
+    maxWidth: "100%",
+    maxHeight: "100%",
+   
   },
   media: {
-    height: 440,
+    height: 420,
+    width: '100%',
+  
+    
   },
+  ancho:{
+    margin: 10,
+    maxWidth: "100%",
+  },
+  btn : {
+    justifyContent: 'center',
+    left: "20%"
+  },
+ 
 });
 
 
@@ -27,6 +43,13 @@ function Draw() {
         lienzo.current.clear() 
     }
 
+    const img = [
+      { photo: ['../../../images/abc1.jpg'] }, 
+      { photo: ['../../../images/abc2.jpg'] }, 
+      { photo: ['../../../images/abc3.jpg'] }, 
+          
+    ];
+
     const style = {
         width: brush + "px",
         background: canvas,
@@ -35,46 +58,27 @@ function Draw() {
       console.log(style.width);
   return (
   <div >
-     <Grid container 
-        
-     >
-            <Grid style={{ textAlign: 'center', alignItems: 'center' }} xs={12} sm={5}>
+     <Grid container>
+            
+            <Grid style={{ textAlign: 'center', alignItems: 'center' }} xs={12} sm={6}>
             <h2>Replica</h2>
-        <Grid style={{ margin: 5, marginTop: 65, position: 'relative', left : 30 }}>
-        <Carousel autoPlay={false} emulateTouch={true}>
-        <div>
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image="../../../images/slide_2.jpg"
-            title="Contemplative Reptile"
-          />
-      </Card>
-        </div>
-        <div>
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image="../../../images/slide_2.jpg"
-            title="Contemplative Reptile"
-          />
-      </Card>
-        </div>
-        <div>
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image="../../../images/slide_2.jpg"
-            title="Contemplative Reptile"
-          />
-      </Card>
-        </div>
-    </Carousel>
+        <Grid style={{ margin: 25, marginTop: 70, position: 'relative',  }}>
+        
+        <Carousel  autoPlay={false} emulateTouch={true}  >
+            { img.map(e =>          
+              <Card className={classes.root}>
+                <CardMedia            
+                  className={classes.media}
+                  image={e.photo}              
+                />
+              </Card>
+          ) }      
+        </Carousel>
+
         </Grid>
        
         </Grid>
-        <Grid sm={1}>
-        </Grid>
+        
         <Grid  xs={12} sm={6}>
         <Grid style={{ position: 'relative', top: 20,  }}>
         
@@ -82,19 +86,18 @@ function Draw() {
         
         <Grid sm={2} >
         <Button onClick={clear}
+        className={classes.btn}
         type="submit"
         fullWidth
         variant="contained"
         color="primary"
-
-        //style={{ borderRadius: 20 }}
       >
         Limpiar
       </Button>
         </Grid>
 
       </Grid>
-            <Card style={{ width: 600, border: '2px solid #14181C', marginTop: 50 }}>
+            <Card className={classes.ancho}  style={{  border: '2px solid #14181C', marginTop: 50 }}>
                 <CanvasDraw
                 ref={lienzo}
                     brushColor={canvas}
