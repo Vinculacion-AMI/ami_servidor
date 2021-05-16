9beee0fbdc82841defef6d3ea5fa3f1ef97442de
DannySinaluisa <dss.flores@yavirac.edu.ec>
2021-05-16 13:12:15 -0500

    borrado de codigo , arreglo de niveles, aumento de algunos nivles

--- provin/src/pages/syllables/data.js
+++ provin/src/pages/syllables/data.js
@@ -36,11 +36,7 @@ export default {
         category: "Bisílabas",
         img: "https://i.ibb.co/BzmcyFX/wordpress-5-limbo.png",
       },
-      {
-        title: "Docente",
-        category: "Trisílabas",
-        img: "https://i.ibb.co/DKrCQZk/docente-profesiones-otras-profesiones-11909455.png",
-      },
+
       {
         title: "carta",
         category: "Bisílabas",
@@ -59,17 +55,41 @@ export default {
         img: "https://i.ibb.co/YtGcTgL/og-img.jpg",
       },
       {
-        title: "Empezar",
-        category: "Trisílabas",
-        img: "https://i.ibb.co/Hh3phN3/Male-Athlete-at-the-starting-line-ready-to-start-the-race.jpg",
-      },
-      {
         title: "Sol",
         category: "Monosílabas",
         img: "https://i.ibb.co/HPJLK2S/sol-jpg-673822677.jpg",
       },
     ],
+    nivel5: [
+      {
+        title: "Perro",
+        category: "Bisílabas",
+        img: "https://www.muyinteresante.com.mx/wp-content/uploads/2018/05/httpstved-prod.adobecqms.netcontentdamtbgmexicomuyinteresantemxcuriosidadestop171225perro.imgo_.jpg",
+      },
+      {
+        title: "Paz",
+        category: "Monosílabas",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8t11bJPNmRdL3y3MwVFoB5aRC1XhUHC5XLA&usqp=CAU",
+      },
+    ],
+    nivel6: [
+      {
+        title: "Arból",
+        category: "Bisílabas",
+        img: "https://fhernanb.github.io/libro_mod_pred/images/arbol.jpg",
+      },
+
+      {
+        title: "gol",
+        category: "Monosílabas",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Soccer_Youth_Goal_Keeper.jpg/330px-Soccer_Youth_Goal_Keeper.jpg",
+      },
+    ],
   },
+
+
+
+  //nivel de bisilibas//
   Bisílabas: {
     nivel1: [
       {
@@ -111,11 +131,6 @@ export default {
         category: "Trisílabas",
         img: "https://i.ibb.co/CHyLLj4/10-38-805x452.jpg",
       },
-      {
-        title: "Sol",
-        category: "Monosílabas",
-        img: "https://i.ibb.co/HPJLK2S/sol-jpg-673822677.jpg",
-      },
     ],
     nivel4: [
       {
@@ -123,11 +138,7 @@ export default {
         category: "Trisílabas",
         img: "https://i.ibb.co/bFftWHv/20130123-104249-a-m.jpg",
       },
-      {
-        title: "Androide",
-        category: "Trisílabas",
-        img: "https://i.ibb.co/48nwsNh/bc54720a7bfcdfb60c11db8136e4e813.jpg",
-      },
+
       {
         title: "Tu",
         category: "Monosílabas",
@@ -140,4 +151,35 @@ export default {
       },
     ],
   },
+  Trisílabas:{
+    nivel1:[
+      {
+        title: "Manzana",
+        category: "Trisílabas",
+        img: "https://i2.wp.com/historiasdelahistoria.com/wordpress-2.3.1-ES-0.1-FULL/wp-content/uploads/2015/09/manzana.jpg?ssl=1",
+      },
+      {
+        title: "cama",
+        category: "Bisílabas",
+        img: "https://i.pinimg.com/736x/2b/7f/76/2b7f768db24d3bd3b7b33beca59decfe.jpg",
+      },
+    ],
+    nivel2:[
+      {
+        title: "doncella",
+        category: "Trisílabas",
+        img: "https://image.freepik.com/vector-gratis/personaje-doncella-nieve-dibujado-mano_23-2148786577.jpg",
+      },
+      {
+        title: "Libro",
+        category: "Bisílabas",
+        img: "https://s1.eestatic.com/2019/07/01/como/libros-estudiantes-universidad_410470522_127023888_1024x576.jpg",
+      },
+      {
+        title: "Bar",
+        category: "Monosílabas",
+        img: "https://image.freepik.com/vector-gratis/bar-interior-composicion-realista_1284-24320.jpg",
+      },
+    ],
+  }
 };
